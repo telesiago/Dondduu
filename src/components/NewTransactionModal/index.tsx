121ed2c6994741e9f317726a5d3298302e0ccb58
Iago Teles <telesiago@gmail.com>
2025-03-28 14:27:24 -0300

    Create accessible radio button

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -21,11 +21,11 @@ import * as Dialog from '@radix-ui/react-dialog';
            <input type="text" placeholder="Categoria" required />
  
            <TransactionType>
-             <TransactionTypeButton variant="income">
+             <TransactionTypeButton variant="income" value='income'>
                <ArrowCircleUp size={24} />
                Entrada
              </TransactionTypeButton>
-             <TransactionTypeButton variant="outcome">
+             <TransactionTypeButton variant="outcome" value='outcome'>
                <ArrowCircleDown size={24} />
                Saída
              </TransactionTypeButton>
